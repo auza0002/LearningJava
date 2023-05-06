84606f85b6031bdc879858c49fa49376d3a483a9
Diego Auza <auza0002@algonquinlive.com>
2023-05-06 08:14:05 -0500

    km / h to mi / h DONE

--- src/JavaChallenges/SpeedConverter.java
+++ src/JavaChallenges/SpeedConverter.java
@@ -2,13 +2,29 @@ package JavaChallenges;
 
 public class SpeedConverter {
     public static void main(String[] args) {
-
+        printConversion(1.5);
+        printConversion(-10);
+        printConversion(0);
+        printConversion(400.45);
+        printConversion(75.677);
+    }
+    public static void printConversion(double kilometersPerHour){
+        long conversion =  toMilesPerHour(kilometersPerHour);
+        if(conversion > -1 ){
+            System.out.println( kilometersPerHour + "km/h" + " = " + conversion + "mi/h");
+        }
+        else {
+            System.out.println("Invalid Value");
+        }
     }
     public static long toMilesPerHour(double kilometersPerHour){
-        long kilometersPerHourResult = -1;
-            if(kilometersPerHour > 0){
-
-            }
-        return kilometersPerHourResult;
+        // 1kph  =  0.62mlong  kilometersPerHourResult;
+        long kilometersPerHourResult;
+        if(kilometersPerHour >= 0){
+            kilometersPerHourResult =  (long)(kilometersPerHour * 0.621371);
+        } else {
+            kilometersPerHourResult = -1;
+        }
+        return Math.round(kilometersPerHourResult);
     }
 }
