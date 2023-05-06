5bfaaafd0989d45ac920be230fd09983829ca826
Diego Auza <auza0002@algonquinlive.com>
2023-05-06 07:07:58 -0500

    positive, negative zero DONE

--- src/JavaChallenges/PositiveNegativeZero.java
+++ src/JavaChallenges/PositiveNegativeZero.java
@@ -2,5 +2,20 @@ package JavaChallenges;
 
 
 public class PositiveNegativeZero {
-
+    public static void main(String[] args) {
+        checkNumber(32);
+        checkNumber(-5444);
+        checkNumber(0);
+    }
+    public static void checkNumber(int checkParam){
+        String checkResult;
+        if(checkParam > 0 ){
+            checkResult = "positive";
+        } else if(checkParam < 0 ){
+            checkResult = "negative";
+        } else {
+            checkResult = "zero";
+        };
+        System.out.println(checkResult);
+    };
 }
