4df2633e9faafcd34c6685459055a4670314640b
Diego Auza <auza0002@algonquinlive.com>
2023-05-06 07:12:30 -0500

    km / h to mi / h

---
+++ src/JavaChallenges/SpeedConverter.java
@@ -0,0 +1,14 @@
+package JavaChallenges;
+
+public class SpeedConverter {
+    public static void main(String[] args) {
+
+    }
+    public static long toMilesPerHour(double kilometersPerHour){
+        long kilometersPerHourResult = -1;
+            if(kilometersPerHour > 0){
+
+            }
+        return kilometersPerHourResult;
+    }
+}
