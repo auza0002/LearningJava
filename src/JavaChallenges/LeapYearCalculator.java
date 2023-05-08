7558d3e397f25e1839243f9f14623e8df75341d2
Diego Auza <auza0002@algonquinlive.com>
2023-05-08 10:39:14 -0500

    Leap year

--- src/JavaChallenges/LeapYearCalculator.java
+++ src/JavaChallenges/LeapYearCalculator.java
@@ -1,4 +1,25 @@
 package JavaChallenges;
 
 public class LeapYearCalculator {
+
+    public static boolean isLeapYear(int year) {
+        if (year < 1 || year > 9999) {
+            return false;
+        }
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
 }
