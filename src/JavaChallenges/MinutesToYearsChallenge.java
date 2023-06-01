3323ee43488c803b6e4095735d3ddb9c2b5cb17b
Diego Auza <auza0002@algonquinlive.com>
2023-06-01 17:46:04 -0500

    minutes challenge completed

--- src/JavaChallenges/MinutesToYearsChallenge.java
+++ src/JavaChallenges/MinutesToYearsChallenge.java
@@ -2,15 +2,26 @@ package JavaChallenges;
 
 public class MinutesToYearsChallenge {
     public static void main(String[] args) {
+        printYearsAndDays(1440);
         printYearsAndDays(525600);
     }
+
+//    Be extra careful about spaces in the printed message.
+//
+//    Use the remainder operator
+//
+//      1 hour = 60 minutes
+//
+//      1 day = 24 hours
+//
+//      1 year = 365 days
      public static void printYearsAndDays(long minutes){
         if(minutes < 0){
             System.out.println("Invalid Value");
         } else {
-            long yearResult = minutes /(43800 * 12);
+            long yearResult = (minutes /1440)/365;
             long minutesResult = minutes;
-            long daysResult = minutes/1440;
+            long daysResult = (minutes /1440)%365;
 
             System.out.println(minutesResult +" min = "+ yearResult + "y and " + daysResult +"d");
         }
