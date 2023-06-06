c92eb5e9bd3337edd5cb31cde33093b84b9b53c0
Diego Auza <auza0002@algonquinlive.com>
2023-06-06 10:43:35 -0500

    Learning java new switch statement

--- src/Switch/Main.java
+++ src/Switch/Main.java
@@ -2,24 +2,26 @@ package Switch;
 
 public class Main {
     public static void main(String[] args) {
-//        int value = 1;
-//        if ( value ==1 ){
-//            System.out.println("value was 1");
-//        }else if( value ==2){
-//            System.out.println("value was 2");
-//        } else {
-//            System.out.println("value was not 1 or 2");
-//        }
+
         int switchValue = 1 ;
-        switch (switchValue){
-            case 1:
-                System.out.println("value was 1");
-                break;
-            case 2 :
-                System.out.println("value was 2");
-                break;
-            default:
-                    System.out.println("the value was not 1 or 2");
+        switch (switchValue) {
+            case 1 -> System.out.println("value was 1");
+            case 2 -> System.out.println("value was 2");
+            default -> System.out.println("the value was not 1 or 2");
         }
+        String month = "APRIL";
+        String month2 = "OCTOBER";
+        System.out.println(month + " is in the " + getQuarter(month) + " quarters");
+        System.out.println(month + " is in the " + getQuarter(month2) + " quarters");
+    }
+
+    public static String getQuarter( String month ){
+        return switch (month) {
+            case "JANUARY", "FEBRUARY", "MARCH" -> "1st";
+            case "APRIL", "MAY", "JUNE" -> "2nd";
+            case "JULY", "AUGUST", "SEPTEMBER" -> "3rd";
+            case "OCTOBER", "NOVEMBER", "DECEMBER" -> "4th";
+            default -> "bad";
+        };
     }
 }
