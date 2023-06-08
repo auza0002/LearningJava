0f040d095f47e8b20512b5ea50b2e0b41a4d7bf4
Diego Auza <auza0002@algonquinlive.com>
2023-06-08 10:50:15 -0500

    prime Number

--- src/Loop/ForLoopChallenge.java
+++ src/Loop/ForLoopChallenge.java
@@ -4,6 +4,9 @@ public class ForLoopChallenge {
     public static void main(String[] args) {
         for (double rate = 7.5 ; rate <= 10.0; rate += 0.25){
             double interestAmount  = calculateInterest(100.0, rate);
+            if(interestAmount > 8.5 ){
+                break;
+            }
             System.out.println(interestAmount);
         }
     }
