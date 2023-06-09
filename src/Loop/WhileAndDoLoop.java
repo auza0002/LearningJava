13078ddb20561aef99dba5a891b0d9bb2515f9d0
Diego Auza <auza0002@algonquinlive.com>
2023-06-09 08:29:53 -0500

    While loop

---
+++ src/Loop/WhileAndDoLoop.java
@@ -0,0 +1,13 @@
+package Loop;
+
+public class WhileAndDoLoop {
+    public static void main(String[] args) {
+        for(int i  = 1 ; i <= 5; i++){
+            System.out.println(i);
+        }
+        int j = 1;
+        while(j <=5){
+            System.out.println(" this the the while loop " + j );
+        }
+    }
+}
