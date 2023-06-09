5723086dcf51f321c39ea5d068276d533b2d7bef
Diego Auza <auza0002@algonquinlive.com>
2023-06-09 16:18:45 -0500

    Input From the Scanner first phase

--- src/ReadingInput/ReadingUserInput.java
+++ src/ReadingInput/ReadingUserInput.java
@@ -1,10 +1,15 @@
-package ReadingInput;
 
+package ReadingInput;
+import java.util.Scanner;
 public class ReadingUserInput {
     public static void main(String[] args) {
-    int currentYear = 2023;
-        System.out.println(getInputFromConsole(currentYear));
-        System.out.println(getInputFromScanner(currentYear));
+        int currentYear = 2023;
+       try {
+           System.out.println(getInputFromConsole(currentYear));
+       } catch (NullPointerException e){
+           System.out.println(getInputFromScanner(currentYear));
+       }
+
     }
     public static String getInputFromConsole(int currentYear){
         String name = System.console().readLine(" Hi, What is your name?");
@@ -14,6 +19,16 @@ public class ReadingUserInput {
         return " So you are " + age + " years old";
     }
     public static String getInputFromScanner(int currentYear){
-        return "";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hi , What is your name?");
+        String name = scanner.nextLine();
+        System.out.println(" Hi " + name + ", This is muy first input from Console.");
+
+        System.out.println("What year were you born");
+        String DateOfBirth = scanner.nextLine();
+        int age = currentYear - Integer.parseInt(DateOfBirth);
+
+        return " So you are " + age + " years old";
     }
 }
