e16f33a18a89d6e187dbaac32a8f4b74dfc92a54
Diego Auza <auza0002@algonquinlive.com>
2023-06-09 11:51:00 -0500

    Palindrome Number Challenge

--- src/JavaChallenges/NumberPalindrome.java
+++ src/JavaChallenges/NumberPalindrome.java
@@ -1,4 +1,25 @@
 package JavaChallenges;
 
 public class NumberPalindrome {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(123456));
+        System.out.println(isPalindrome(-1221));
+    }
+    public static boolean  isPalindrome( int number){
+        boolean result = false;
+        int original = number;
+        int reverse = 0;
+        while (number != 0) {
+            int lastDigit = number % 10;
+            reverse = (reverse * 10) + lastDigit;
+            number = number / 10;
+        }
+
+        if (original == reverse) {
+            result =  true;
+        }
+
+        return result;
+
+    }
 }
