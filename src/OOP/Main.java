c997faac80fa556719b636abdfcbf87bc84839df
Diego Auza <diegoauza@Diegos-MacBook-Pro.local>
2023-07-05 08:57:37 -0500

    car class

--- src/OOP/Main.java
+++ src/OOP/Main.java
@@ -3,9 +3,21 @@ package OOP;
 public class Main {
     public static void main(String[] args){
         Car car  = new Car();
-        System.out.println("make" + car.getMake());
-        System.out.println("make" + car.getColor());
+        car.setMake("porsche");
+        car.setModel("new porsche");
+        car.setDoors(2);
+        car.setConvertible(true);
+        car.setColor("Black");
+        // describe the car
         car.describeCar();
 
+        Car targa = new  Car();
+        targa.setMake("holden");
+        targa.setModel("new targe");
+        targa.setDoors(4);
+        targa.setConvertible(false);
+        targa.setColor("red");
+        // describe the car
+        targa.describeCar();
     }
 }
