3e1fe357e90d5dd7e5ca135e3bffa24365807e64
Diego Auza <diegoauza@Diegos-MacBook-Pro.local>
2023-07-05 08:01:34 -0500

    car setter and getters

--- src/OOP/Car.java
+++ src/OOP/Car.java
@@ -1,11 +1,31 @@
 package OOP;
 
 public class Car {
-    private String make ;
-    private String model ;
-    private String color;
+    private String make = "Tesla" ;
+    private String model = "Model X" ;
+    private String color = "Red";
     private int doors;
     private boolean convertible;
+
+    public String getMake(){
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
 
     public void describeCar(){
         System.out.println(doors + " -Door " +
--- src/OOP/Main.java
+++ src/OOP/Main.java
@@ -3,8 +3,9 @@ package OOP;
 public class Main {
     public static void main(String[] args){
         Car car  = new Car();
+        System.out.println("make" + car.getMake());
+        System.out.println("make" + car.getColor());
         car.describeCar();
-        // car
-        //
+
     }
 }
