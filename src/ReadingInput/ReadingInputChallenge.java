95618ee5d068f1ea2dc201ddaf64f731a1a7a185
Diego Auza <diegoauza@Diegos-MacBook-Pro.local>
2023-07-04 20:09:09 -0500

    OOP classe

--- src/ReadingInput/ReadingInputChallenge.java
+++ src/ReadingInput/ReadingInputChallenge.java
@@ -1,7 +1,21 @@
 package ReadingInput;
+import java.util.Scanner;
 
 public class ReadingInputChallenge {
     public static void main(String[] args) {
-        // this is a comment that is used to make a git commit
+        Scanner scanner = new Scanner(System.in);
+        int counter =1 ;
+        int sum = 0;
+        while (counter <= 5){
+            System.out.println("Enter number #" + counter + ":");
+            String nextNumber  = scanner.nextLine();
+            try {
+                int number  = Integer.parseInt(nextNumber);
+                counter ++;
+            } catch (NumberFormatException nfe){
+                System.out.println("invalid number");
+            }
+
+        }
     }
 }
