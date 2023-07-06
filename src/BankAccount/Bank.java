1f518304e6b86efe4da8d419f1fd199374dfa77f
Diego Auza <auza0002@algonquinlive.com>
2023-07-06 13:50:30 -0500

    new constructor

--- src/BankAccount/Bank.java
+++ src/BankAccount/Bank.java
@@ -9,6 +9,19 @@ private String customerName;
 private String customerEmail;
 private String customerPhone;
 
+public Bank(){
+    System.out.println("Empty constructor called");
+}
+public Bank( String number, double balance, String customerName, String customerEmail, String customerPhone){
+    System.out.println(" Account constructor with parameters");
+    this.number = number;
+    this.balance = balance;
+    this.customerName = customerName;
+    this.customerEmail = customerEmail;
+    this.customerPhone = customerPhone;
+
+}
+
 public void depositFound (double depositAmount){
     balance += depositAmount;
     System.out.println(" Deposit of $" + depositAmount + " made. New balance is $" + balance);
@@ -63,6 +76,7 @@ public void withDrawingAmount( double drawingAmount){
     }
     public void bankDetails(){
         System.out.println(" Name: " + customerName);
+        System.out.println(" Number: " + number);
         System.out.println(" Balance: $" + balance);
         System.out.println(" Email: "+ customerEmail);
         System.out.println(" Phone Number :" + customerPhone);
--- src/BankAccount/Main.java
+++ src/BankAccount/Main.java
@@ -2,11 +2,11 @@ package BankAccount;
 
 public class Main {
     public static void main(String[] args) {
-            Bank bank = new Bank();
-            bank.setCustomerName("Diego Luis Auza Trujillo");
-            bank.setBalance(1000.00);
-            bank.setCustomerEmail("auzajuan1@gmail.com");
-            bank.setCustomerPhone("3118999898Bala");
+            Bank bank = new Bank("12345", 1000.00 , "Diego Brown", "myemaul@bob.com", "(087) 123-456");
+//            bank.setCustomerName("Diego Luis Auza Trujillo");
+//            bank.setBalance(1000.00);
+//            bank.setCustomerEmail("auzajuan1@gmail.com");
+//            bank.setCustomerPhone("3118999898Bala");
             bank.bankDetails();
             bank.withDrawingAmount(100.0);
             bank.depositFound(250);
