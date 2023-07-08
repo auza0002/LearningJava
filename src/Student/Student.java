3971d0d67ed617dcfd5f6d4d0ff2f3e4d69c47c4
Diego Auza <auza0002@algonquinlive.com>
2023-07-08 08:57:23 -0500

    Student generator

---
+++ src/Student/Student.java
@@ -0,0 +1,15 @@
+package Student;
+
+public class Student {
+    private String id;
+    private String name;
+    private String dataOfBirth;
+    private String classList;
+
+    public Student(String id, String name, String dataOfBirth, String classList) {
+        this.id = id;
+        this.name = name;
+        this.dataOfBirth = dataOfBirth;
+        this.classList = classList;
+    }
+}
