67480da544bfb0d0548a42315b93a78b5e204fa0
Diego Auza <auza0002@algonquinlive.com>
2023-07-18 15:49:44 -0500

    LPAStudents

---
+++ src/Student/LPAStudent.java
@@ -0,0 +1,5 @@
+package Student;
+
+public record LPAStudent(String id, String name, String dataOfBirth, String classList) {
+
+}
--- src/Student/Main.java
+++ src/Student/Main.java
@@ -3,7 +3,7 @@ package Student;
 public class Main {
     public static void main(String[] args) {
         for (int i = 1 ; i <= 5 ; i ++){
-            Student s = new Student("S92300" +  i , switch (i){
+            LPAStudent s = new LPAStudent("S92300" +  i , switch (i){
                 case 1 -> "Mary";
                 case 2 -> "Carol";
                 case 3 -> "Tim";
@@ -14,5 +14,17 @@ public class Main {
                     "05/11/1985", " Java MasterClass");
             System.out.println(s);
         }
+        Student pojoStudent  = new Student ("S923006" , "Ann", "05/11/1985", "Java MasterClass");
+        LPAStudent recordStudent = new LPAStudent("S93007", "Bill", "05/11,1985", "Java MasterClass");
+
+        System.out.println(pojoStudent);
+        System.out.println(recordStudent);
+
+        pojoStudent.setClassList(pojoStudent.getClassList() + " ,Java OCp Exam 829");
+//        recordStudent.setClassList(recordStudent.classList() + " , Java OCp Exam 829");
+
+        System.out.println(pojoStudent.getName() + " is taking" + pojoStudent.getClassList());
+
+        System.out.println(recordStudent.name() + " is taking" + recordStudent.classList());
     }
 }
