beccf9bd5f55f8fcd1ec76907dbff470ad7df170
Diego Auza <auza0002@algonquinlive.com>
2023-07-18 16:18:22 -0500

    Wall challenge

---
+++ src/WallArea/Main.java
@@ -0,0 +1,13 @@
+package WallArea;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Wall wall =  new Wall(-10 ,4);
+
+        System.out.println(wall);
+    }
+}
