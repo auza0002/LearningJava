13b36716e0d423ab74dbd2f2c5c8bbfd8e113857
Diego Auza <auza0002@algonquinlive.com>
2023-07-19 14:58:04 -0500

    complex number

---
+++ src/ComplexNumber/ComplexNumber.java
@@ -0,0 +1,23 @@
+package ComplexNumber;
+
+public class ComplexNumber {
+     private double real;
+     private double imaginary;
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public void Add(double real, double imaginary){
+
+    }
+}
