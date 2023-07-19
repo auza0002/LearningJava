40c0696185fa7cae4d5dfa60768b2ecde832a9bc
Diego Auza <auza0002@algonquinlive.com>
2023-07-19 12:24:04 -0500

    CarpetCostCalculator

---
+++ src/CarpetCostCalculator/Floor.java
@@ -0,0 +1,28 @@
+package CarpetCostCalculator;
+
+public class Floor {
+    double width;
+    double length;
+
+    public Floor() {
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public Floor(double width, double length) {
+        if(width < 0) this.width = 0;
+        if(length < 0) this.length = 0;
+    }
+    public double getArea(){
+        double total = this.length * this.width;
+        return total;
+    }
+
+}
+
---
+++ src/CarpetCostCalculator/Main.java
@@ -0,0 +1,8 @@
+package CarpetCostCalculator;
+
+public class Main {
+    public static void main(String[] args) {
+        Floor floor = new Floor(-1, -8);
+        System.out.println(floor);
+    }
+}
