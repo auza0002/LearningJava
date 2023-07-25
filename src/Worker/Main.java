72f167261cc4c8e496bbefb9a2b8090df645b59f
Diego Auza <auza0002@algonquinlive.com>
2023-07-25 07:55:14 -0500

    The TExt Block

--- src/Worker/Main.java
+++ src/Worker/Main.java
@@ -8,6 +8,16 @@ public class Main {
         System.out.println(tim);
         System.out.println("Age " + tim.getAge());
         System.out.println("Pay = "+ tim.collectPay());
+        SalariedEmployee joe = new SalariedEmployee("Joe", "11/11/1990", "03/03/2020", 35000);
+        System.out.println(joe);
+        System.out.println("joe's Paycheck = $" + joe.collectPay());
+        joe.retired();
+        System.out.println("Joe's pension check is = " + joe.collectPay());
+
+        HourlyEmployee mary  = new HourlyEmployee( "Mary", "05/05/1970", "03/03/2021", 15);
+        System.out.println(mary);
+        System.out.println("Mary's pay check = $ " + mary.collectPay());
+        System.out.println("Mary's  pay check  is = $ " + mary.getDoublePay());
     }
 }
 class Worker{
@@ -60,6 +70,41 @@ class Employee extends Worker{
                 "employeeId=" + employeeId +
                 ", hireDate='" + hireDate + '\'' +
                 "} " + super.toString();
+    }
+}
+class SalariedEmployee extends Employee{
+        double annualSalary;
+        boolean isRetired;
+
+    public SalariedEmployee(String name, String birthDate, String hireDate, double annualSalary) {
+        super(name, birthDate, hireDate);
+        this.annualSalary = annualSalary;
+    }
+
+    @Override
+    public double collectPay(){
+        double paycheck = annualSalary / 26;
+        double adjustedPay = (isRetired) ? 0.9 * paycheck : paycheck;
+        return (int) adjustedPay;
+    }
+    public void retired(){
+        terminate("12/12/2025");
+        isRetired = true;
+    }
+}
+class HourlyEmployee extends Employee{
+    private double hourlyPayRate;
+
+    public HourlyEmployee(String name, String birthDate, String hireDate, double hourlyPayRate) {
+        super(name, birthDate, hireDate);
+        this.hourlyPayRate = hourlyPayRate;
+    }
+    @Override
+    public double collectPay(){
+        return 40* hourlyPayRate;
+    }
+    public double getDoublePay (){
+        return 2 * collectPay();
     }
 }
 
