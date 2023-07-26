76f3b79437b9a20d2e1a687ab0c3178c16fad652
Diego Auza <auza0002@algonquinlive.com>
2023-07-26 10:09:13 -0500

    string methods

--- src/AllAboutStrings/Main.java
+++ src/AllAboutStrings/Main.java
@@ -12,8 +12,22 @@ public class Main {
         System.out.printf("Index of l las index of = %d %n", helloWorld.lastIndexOf("l"));
         System.out.printf("Index of l = %d %n", helloWorld.indexOf("l",  3));
         System.out.printf("Index of l las index of = %d %n", helloWorld.lastIndexOf("l" , 8));
-        System.out.printf("Index of world = %d %n", helloWorld.indexOf("World"));
+        System.out.printf("Index of world = %d %n ", helloWorld.indexOf("World"));
 
+        String helloWorldLower = helloWorld.toLowerCase();
+        if(helloWorld.equals(helloWorldLower)){
+            System.out.println("Values match exactly");
+        }  if(helloWorld.equalsIgnoreCase(helloWorldLower)){
+            System.out.println("Values match ignoring case ");
+        }
+    if(helloWorld.startsWith(("Hello"))){
+        System.out.println("\n String starts with Hello");
+    }
+    if(helloWorld.endsWith(("World"))){
+        System.out.println("String ends with Hello");
+    } if(helloWorld.contains("World")){
+        System.out.println("String contains World");
+    }
     }
     public static void printInformation (String string){
         int length = string.length();
